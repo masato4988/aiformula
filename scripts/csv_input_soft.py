27d3242b4bacd6598bc8e4e05711d6d302304f40
rdclab <s24s1037hw@s.chibakoudai.jp>
2025-03-17 19:12:56 +0900

    fix

--- scripts/csv_input_soft.py
+++ scripts/csv_input_soft.py
@@ -12,7 +12,6 @@ class Converter(Node):
 
         self.csv_file = open('shihou_gnssnav.csv', mode='w', newline='')
         self.csv_writer = csv.writer(self.csv_file)
-        self.csv_writer.writerow(['Latitude', 'Longitude'])
 
         self.counter = 0  # カウンタを初期化
 
